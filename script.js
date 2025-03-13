55339f9a8ef1cc27f98fea090056dd686593e1dd
morenorpl <morenoibrahim606@gmail.com>
2025-03-13 17:22:05 +0700

    popup penyakit + responsive dikit

---
+++ script.js
@@ -0,0 +1,39 @@
+// Function to open popup
+function openPopup() {
+    document.getElementById("popup").style.display = "flex";
+}
+
+// Function to close popup
+function closePopup() {
+    document.getElementById("popup").style.display = "none";
+}
+
+// Function to open popup
+function openPopup2() {
+    document.getElementById("popup2").style.display = "flex";
+}
+
+// Function to close popup
+function closePopup2() {
+    document.getElementById("popup2").style.display = "none";
+}
+
+// Function to open popup
+function openPopup3() {
+    document.getElementById("popup3").style.display = "flex";
+}
+
+// Function to close popup
+function closePopup3() {
+    document.getElementById("popup3").style.display = "none";
+}
+
+// Function to open popup
+function openPopup4() {
+    document.getElementById("popup4").style.display = "flex";
+}
+
+// Function to close popup
+function closePopup4() {
+    document.getElementById("popup4").style.display = "none";
+}
