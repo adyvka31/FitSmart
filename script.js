0f9cfa8cbf45d28c055264d01c7031e0d2017821
khalilpuribawa <khalilpuribawa80@gmail.com>
2025-03-14 12:59:07 +0700

    khalil done

--- script.js
+++ script.js
@@ -37,3 +37,182 @@ function openPopup4() {
 function closePopup4() {
     document.getElementById("popup4").style.display = "none";
 }
+
+
+function openPopup5() {
+    document.getElementById("popup5").style.display = "flex";
+}
+
+// Function to close popup
+function closePopup5() {
+    document.getElementById("popup5").style.display = "none";
+}
+
+function openPopup6() {
+    document.getElementById("popup6").style.display = "flex";
+}
+
+// Function to close popup
+function closePopup6() {
+    document.getElementById("popup6").style.display = "none";
+}
+
+function openPopup7() {
+    document.getElementById("popup7").style.display = "flex";
+}
+
+// Function to close popup
+function closePopup7() {
+    document.getElementById("popup7").style.display = "none";
+}
+
+function openPopup8() {
+    document.getElementById("popup8").style.display = "flex";
+}
+
+// Function to close popup
+function closePopup8() {
+    document.getElementById("popup8").style.display = "none";
+}
+
+function openPopup9() {
+    document.getElementById("popup9").style.display = "flex";
+}
+
+// Function to close popup
+function closePopup9() {
+    document.getElementById("popup9").style.display = "none";
+}
+
+function openPopup10() {
+    document.getElementById("popup10").style.display = "flex";
+}
+
+// Function to close popup
+function closePopup10() {
+    document.getElementById("popup10").style.display = "none";
+}
+
+function openPopup11() {
+    document.getElementById("popup11").style.display = "flex";
+}
+
+// Function to close popup
+function closePopup11() {
+    document.getElementById("popup11").style.display = "none";
+}
+
+function openPopup12() {
+    document.getElementById("popup12").style.display = "flex";
+}
+
+// Function to close popup
+function closePopup12() {
+    document.getElementById("popup12").style.display = "none";
+}
+
+function openPopup13() {
+    document.getElementById("popup13").style.display = "flex";
+}
+
+// Function to close popup
+function closePopup13() {
+    document.getElementById("popup13").style.display = "none";
+}
+
+function openPopup14() {
+    document.getElementById("popup14").style.display = "flex";
+}
+
+// Function to close popup
+function closePopup14() {
+    document.getElementById("popup14").style.display = "none";
+}
+
+function openPopup15() {
+    document.getElementById("popup15").style.display = "flex";
+}
+
+// Function to close popup
+function closePopup15() {
+    document.getElementById("popup15").style.display = "none";
+}
+
+function openPopup16() {
+    document.getElementById("popup16").style.display = "flex";
+}
+
+// Function to close popup
+function closePopup16() {
+    document.getElementById("popup16").style.display = "none";
+}
+
+function openPopup17() {
+    document.getElementById("popup17").style.display = "flex";
+}
+
+// Function to close popup
+function closePopup17() {
+    document.getElementById("popup17").style.display = "none";
+}
+
+function openPopup18() {
+    document.getElementById("popup18").style.display = "flex";
+}
+
+// Function to close popup
+function closePopup18() {
+    document.getElementById("popup18").style.display = "none";
+}
+function openPopup19() {
+    document.getElementById("popup19").style.display = "flex";
+}
+
+// Function to close popup
+function closePopup19() {
+    document.getElementById("popup19").style.display = "none";
+}
+function openPopup20() {
+    document.getElementById("popup20").style.display = "flex";
+}
+
+// Function to close popup
+function closePopup20() {
+    document.getElementById("popup20").style.display = "none";
+}
+
+function openPopup21() {
+    document.getElementById("popup21").style.display = "flex";
+}
+
+// Function to close popup
+function closePopup21() {
+    document.getElementById("popup21").style.display = "none";
+}
+
+function openPopup22() {
+    document.getElementById("popup22").style.display = "flex";
+}
+
+// Function to close popup
+function closePopup22() {
+    document.getElementById("popup22").style.display = "none";
+}
+
+function openPopup23() {
+    document.getElementById("popup23").style.display = "flex";
+}
+
+// Function to close popup
+function closePopup23() {
+    document.getElementById("popup23").style.display = "none";
+}
+
+function openPopup24() {
+    document.getElementById("popup24").style.display = "flex";
+}
+
+// Function to close popup
+function closePopup24() {
+    document.getElementById("popup24").style.display = "none";
+}
